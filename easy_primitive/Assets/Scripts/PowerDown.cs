82703636e60cf9543e03b0b6aae4f559372aa338
edulol-1 <edu3000@ciencias.unam.mx>
2025-02-23 19:53:08 -0600

    Add color change for SphereDecrease prefab

--- easy_primitive/Assets/Scripts/PowerDown.cs
+++ easy_primitive/Assets/Scripts/PowerDown.cs
@@ -17,13 +17,26 @@ public class PowerDown : MonoBehaviour
 
     IEnumerator superVelocity(GameObject player)
     {
+        // Hacemos referencia al renderer del jugador
+        Renderer playerRenderer = player.GetComponentInChildren<Renderer>();
+        if (playerRenderer == null)
+        {
+            Debug.LogError("No renderer found in player object :(");
+            yield break;
+        }
+        // Guardamos el color original y cambiamos el color temporalmente
+        Color originalColor = playerRenderer.material.color;
+        playerRenderer.material.color = Color.red;
+
         // Apagamos el objecto
         this.gameObject.GetComponent<MeshRenderer>().enabled = false;
         this.gameObject.GetComponent<SphereCollider>().enabled = false;
-        // this.gameObject.SetActive(false);
+
         player.GetComponent<CharacterMovement>().setVelocity(1.5f);
-        // Devolvemos valor temporal, primero esperamos 3 segundos y luego regresamos.
+        
+        // Devolvemos la velocidad y el color original, primero esperamos 3 segundos y luego regresamos.
         yield return new WaitForSeconds(2.0f);
+        playerRenderer.material.color = originalColor;
         player.GetComponent<CharacterMovement>().resetVelocity();
         Destroy(this.gameObject);
     }
